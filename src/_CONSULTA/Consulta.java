c1d0bd3e46fb1f4e3dc8c964b3628629573c7f4c
Fernando Perez <fernando.s256217@cesurformacion.com>
2022-02-16 20:30:17 +0100

    test con clase Cliente

--- src/_CONSULTA/Consulta.java
+++ src/_CONSULTA/Consulta.java
@@ -1,9 +1,24 @@
 package _CONSULTA;
+import java.util.ArrayList;
+
+import clases.Cliente;
 
 public class Consulta {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ArrayList <Cliente> visitasDiarias = new ArrayList<Cliente>();
+		int maxVisitas=15;
+		int minVisitas=10;
+		
+		for (int i=((int)Math.random()*(maxVisitas-minVisitas+1)+minVisitas); i<maxVisitas;i++) {
+			visitasDiarias.add(new Cliente());
+		}
+		for(int i=0; i<visitasDiarias.size();i++) {
+			System.out.printf(visitasDiarias.get(i).getNombre()+" ");
+			System.out.println(visitasDiarias.get(i).getApellidos());
+		}
 
 	}
 
